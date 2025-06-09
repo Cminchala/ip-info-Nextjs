f05a43ea9081c6ae196891f0ddf4b5d1efdbedd8
Cminchala <christianminchala97@gmail.com>
2025-06-09 18:49:55 -0400

    added various methods

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 'use client';
+import { get } from "http";
 import Image from "next/image";
 
 import { useState , useEffect } from "react";
+import Map from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Home() {
   const [ip, setIp] = useState<string | null>(null);
@@ -38,21 +41,47 @@ export default function Home() {
     fetchIpInfo();
   }, []);
 
-  
 
-  return (
-    <div className="flex items-center justify-center min-h-screen">
+    const [location , setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
+      }
+      ,
+      (error) => {
+        console.error('Error getting location:', error);
+      }
+    );
+  }, []);
+  
+return (
+  <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center space-y-6">
       <h1>Your IP Address: {ip}</h1>
-      
+
       {ipinfo && (
-      <div className="mt-4">
-        <p>Country: {ipinfo.countryName}</p>
-        <p>Region: {ipinfo.regionName}</p>
-        <p>City: {ipinfo.cityName}</p>
-        <p>ZipCode: {ipinfo.zipCode}</p>
-      </div>
+        <div>
+          <p>Country: {ipinfo.countryName}</p>
+          <p>Region: {ipinfo.regionName}</p>
+          <p>City: {ipinfo.cityName}</p>
+          <p>ZipCode: {ipinfo.zipCode}</p>
+        </div>
       )}
+
+      {location && (
+        <div>
+          <h5 className="text-sm font-bold">
+            Your Location from browser using navigator.geolocation.getCurrentPosition
+          </h5>
+          <h5 className="text-sm font-bold">[Allow First From Browser]</h5>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+        </div>
+      )}
+      
     </div>
-    
-  );
+  </div>
+);
 }
